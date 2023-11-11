35b86a5b3b635e6b3e10415684a446d6fce82332
not-g2 <oommenjittu@gmail.com>
2023-11-11 23:09:07 +0530

    Added Links for Different Parts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import NavBar from './Components/NavBar'
 import Home from './Pages/Home';
-import About_Us from './Pages/About_Us';
+import AboutUs from './Pages/About_Us';
 import Pricing from './Pages/Pricing';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Pricing/>
+      <Routes>
+        <Route path='/home' element = {<Home/>}/>
+        <Route path='/about' element = {<AboutUs/>}/>
+        <Route path='/pricing' element = {<Pricing/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/Welcome.js
+++ frontend/src/Components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Welcome = () => {
   return (
     <div>
-      <h1>Welcome User</h1>
+      <h1>Welcome</h1>
     </div>
   )
 }
