b8188ab3610c6d4276ba3bd16a5559eaaa729051
AlbinSiby1 <albinsiby234@gmail.com>
2023-11-12 12:04:48 +0530

    Fixed home

--- frontend/src/Components/MainHeader.js
+++ frontend/src/Components/MainHeader.js
@@ -11,10 +11,10 @@ const MainHeader = () => {
 
 
         <ul className="nav col-4 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="./home.html" className="nav-link px-2 text-white ">Home</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+          <li><a href="./home" className="nav-link px-2 text-white ">Home</a></li>
+          <li><a href="./pricing" className="nav-link px-2 text-white">Pricing</a></li>
           <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">About_Us</a></li>
+          <li><a href="./about" className="nav-link px-2 text-white">About_Us</a></li>
         </ul>
 
 
