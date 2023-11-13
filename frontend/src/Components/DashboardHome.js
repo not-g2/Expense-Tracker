0b6a86fedba1e4c652e5faf2316e5cb37fb70dcf
AlbinSiby1 <albinsiby234@gmail.com>
2023-11-13 17:23:45 +0530

    added graph

--- frontend/src/Components/DashboardHome.js
+++ frontend/src/Components/DashboardHome.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Welcome from './Welcome'
 import QuickSummary from './QuickSummary'
+import Pie from './graph'
+import UserData from './Dashboardpiedata'
 
 const DashboardHomePage = () => {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
   return (
     <div>
         <div className='dashboard-header'>
@@ -10,9 +30,12 @@ const DashboardHomePage = () => {
         </div>
         <div className='dashboard-body'>
           <QuickSummary/>
+          <div style={{ width: 500 }}>
+        <Pie chartData={userData} />
+        </div>
         </div>
     </div>
   )
-}
+  }
 
 export default DashboardHomePage
